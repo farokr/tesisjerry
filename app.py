80372a151ccfe62053b9e41e34e7d961735502cb
Faro KR <faro.rasyid@gmail.com>
2021-08-12 05:27:32 +0700

    Source Code Script Streamlit

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
+from kmodes.kmodes import KModes
+
 import pickle
 import base64
 
@@ -236,6 +238,107 @@ def kmeans():
     
 #end of kmeans
 
+# bagian menu K-MODES 
+def kmodes():
+    st.header('K-Modes')
+    
+    df_master = pd.read_csv('data_siswa.csv',sep=';')     
+    df = df_master.copy()
+    df = proses_data(df)
+    pca = PCA(2) #mengubah menajdi 2 kolom
+    df = pca.fit_transform(df) #Transform data
+    
+    
+    st.subheader('Pemilihan nilai K Menggunakan DBI')
+    cost = []
+    slh = []
+    dbi = []
+    K = range(2,11)
+
+    for k in K:
+        kmode = KModes(n_clusters=k, init = "Cao", n_init = 1)
+        kmode.fit_predict(df)
+        #evaluasi elbow
+        cost.append(kmode.cost_)
+        #evaluasi silhouette
+        slh.append(metrics.silhouette_score(df,kmode.labels_))
+        #evaluasi DBI
+        dbi.append(metrics.davies_bouldin_score(df,kmode.labels_))
+        
+    fig= plt.figure()
+    plt.plot(K, dbi, 'bx-')
+    plt.xlabel('Values of K')
+    plt.ylabel('DBI Score')
+    plt.title('DBI Validation Method')
+    st.write(fig)
+    
+    tmp = pd.DataFrame()
+    tmp['K'] = K
+    tmp['dbi'] = dbi
+    st.write(tmp)
+    
+    
+    st.subheader('K optimal = 4')
+    
+    st.header('Simulasi K-Modes Model')
+    k_value  = st.slider('Nilai K', min_value=2, max_value=10, step=1, value=4)
+    
+    model = KModes(n_clusters=k_value, init = "Cao", n_init = 1, verbose=0,random_state=101)
+    label = model.fit_predict(df) #proses Clustering
+    center = model.cluster_centroids_
+    
+    #dibuat menjadi dataFrame
+    df_master['x1'] = df[:,0]
+    df_master['y1'] = df[:,1]
+    df_master['cluster'] = label
+    
+    cluster = df_master['cluster'].unique()
+    cluster.sort()
+    
+    fig3= plt.figure()
+    ax = sns.scatterplot(x='x1', y='y1',hue='cluster',data=df_master,alpha=1, s=40, palette=palet)
+    ax = sns.scatterplot(x=center[:, 0], y=center[:, 1],hue=range(k_value), s=100, palette=palet, ec='black',label='centroid',legend=False)
+    plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+    st.write(fig3)
+    
+    fig4= plt.figure()
+    ax = sns.countplot(x ='cluster', data=df_master,palette=palet)
+    for p in ax.patches:
+        _x = p.get_x() + p.get_width() / 2
+        _y = p.get_y() + p.get_height()
+        value = int(p.get_height())
+        ax.text(_x, _y, value, ha="center")   
+    st.write(fig4)
+    
+    st.subheader('Profil Data per Cluster')
+    arr_cluster = {}
+    for x in cluster:
+        clu = df_master.loc[df_master['cluster'] == x]
+        idx = 'cluster '+str(x)
+        arr_mode = arr_cluster[idx] = {}
+        arr_mode['jenis kelamin'] = clu['jenis kelamin'].mode().iat[0]
+        arr_mode['kota lahir'] = clu['kota lahir'].mode().iat[0]
+        arr_mode['kecamatan'] = clu['kecamatan'].mode().iat[0]
+        arr_mode['agama'] = clu['agama'].mode().iat[0]
+        arr_mode['angkatan'] = clu['angkatan'].mode().iat[0]
+        arr_mode['tahun raport 3'] = clu['tahun raport 3'].mode().iat[0]
+        arr_mode['bidang studi keahlian'] = clu['bidang studi keahlian'].mode().iat[0]
+        arr_mode['program studi keahlian'] = clu['program studi keahlian'].mode().iat[0]
+        arr_mode['kompetensi keahlian'] = clu['kompetensi keahlian'].mode().iat[0]
+        arr_mode['asal sekolah'] = clu['asal sekolah'].mode().iat[0]
+
+    df_cluster = pd.DataFrame(arr_cluster)
+    st.dataframe(df_cluster)    
+
+    st.subheader('Pilih Cluster')
+    choice = st.selectbox("",cluster)
+    res = df_master.loc[df_master['cluster'] == choice]
+    st.subheader('Cluster '+str(choice)+': '+str(res.shape[0]))
+    st.write(res)    
+
+
+#end of kmodes
+
 # bagian menu APLIKSI PERHITUNGAN
 def apps():
     #k_value = int(st.text_input('Nilai K:',value=3))
@@ -318,6 +421,8 @@ def apps():
         
 #end of apps
 
+
+
 # bagian menu BERANDA
 def home():
     #st.title('Download')
@@ -340,10 +445,12 @@ def home():
 #end of home  
     
 def main():
-    """ Streamlit Pelanggaran Pilkada Jabar """
+    
+    #ubah tanda '#' pada  activities kalau mau memasukkan/menghilangkan menu K-modes
+    
+    activities = ['Beranda','Dataset Siswa','K-Means','K-Modes','Aplikasi Perhitungan']
+    #activities = ['Beranda','Dataset Siswa','K-Means','Aplikasi Perhitungan']
 
-    activities = ['Beranda','Dataset Siswa','K-Means','Aplikasi Perhitungan']
-    #choice = st.sidebar.selectbox("Select Activities",activities)
     st.sidebar.subheader("Menu")
     choice = st.sidebar.radio('',activities)
     if choice == 'Beranda':
@@ -352,6 +459,8 @@ def main():
         eda()
     elif choice == 'K-Means':
         kmeans()
+    elif choice == 'K-Modes':
+        kmodes()
     elif choice == 'Aplikasi Perhitungan':
         apps()
         
